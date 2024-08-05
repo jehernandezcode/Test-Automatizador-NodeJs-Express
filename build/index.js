ebb32584dd2dae9fd5cc4a7ff4a8f0af18467e3c
jehernandezcode <jhonnhernandez754@gmail.com>
2024-08-05 16:20:06 -0500

    fix: :see_no_evil: add folder build at gitignore

--- build/index.js
+++ build/index.js
@@ -4,14 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const products_route_1 = __importDefault(require("./routes/products.route"));
+const database_1 = require("./database/database");
+const config_1 = require("./config/config");
 const app = (0, express_1.default)();
-const PORT = 3000;
 // middleware
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.get("/", (_req, res) => {
-    res.send("Hello from Node API Server Updated");
+    res.send("Home Api");
 });
-app.listen(PORT, () => {
-    console.log(`Server listen on port ${PORT}`);
+app.use("/api/products", products_route_1.default);
+//database
+(0, database_1.connectDB)()
+    .then(() => {
+    app.listen(config_1.PORT || 3000, () => {
+        console.log(`Server listen on port ${config_1.PORT || 3000}`);
+    });
+})
+    .catch(() => {
+    console.log("Connection database failed!");
 });
